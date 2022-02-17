d7af93003690c62dafd11bb05e2b98f6b327af3d
Reaashton <rgregory01@mynbcc.ca>
2022-02-17 02:01:42 -0400

    added additional quotes

--- get-quote.py
+++ get-quote.py
@@ -4,6 +4,14 @@ def primary():
   f = open("quotes.txt")
   quotes = f.readlines()
   f.close()
+  str1 = " 'hello' "
+  print(str1)
+
+  str2 = " 'world' "
+  print (str2)
+
+  str3  = str1 + str2
+  print(str3)
 
   last = 13
   rnd = random.randint(0, last)
